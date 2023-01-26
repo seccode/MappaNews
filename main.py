6d6bd8f9abd1cfa3047670e2fba8e485d2626ee5
seccode <42663376+seccode@users.noreply.github.com>
2023-01-26 08:24:32 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import collections,random,requests,tqdm
 from PIL import Image, ImageDraw, ImageFont
+from chatgpt_wrapper import ChatGPT
+bot=ChatGPT()
 
 shortcuts={
 "dc":"Washington D.C.",
@@ -12,7 +14,7 @@ locs=set()
 
 n_key="761b840832514c7fa005c8f9d78c0693"
 g_key="AIzaSyA_xpbVEp9woLniuF8Cl-2TG80wkte6JiU"
-url=("https://newsapi.org/v2/top-headlines?country=us&apiKey="+n_key)
+url=("https://newsapi.org/v2/everything?q=e&searchIn=content&apiKey="+n_key)
 r=requests.get(url)
 s=open("template.html").read()
 a=r.json()["articles"]
@@ -20,7 +22,7 @@ for i,article in tqdm.tqdm(enumerate(a),total=len(a)):
 	n=article["source"]["name"]
 	t=article["title"]
 	u=article["url"]
-	location=input(n+"||"+t+"\n")
+	location=input(t+"\n")
 	if location in shortcuts:
 		location=shortcuts[location]
 	url=("https://maps.googleapis.com/maps/api/geocode/json?address="+location+"&key="+g_key)
@@ -38,7 +40,7 @@ for i,article in tqdm.tqdm(enumerate(a),total=len(a)):
 	articles[n].append((t,u,l))
 	s=s.replace("C"+str(i)+"\n","["+str(l[1])+","+str(l[0])+"]"+"\n")
 	t=t.replace('"',"'")
-	s=s.replace("S"+str(i)+"\n",'"'+t[:20]+"..."+'"'+"\n")
+	s=s.replace("S"+str(i)+"\n",'"'+n+": "+t[:18]+"..."+'"'+"\n")
 	s=s.replace("U"+str(i)+",",'"'+u+'",')
 f=open("index.html","w")
 f.write(s)
