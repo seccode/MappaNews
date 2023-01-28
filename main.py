befa907d6ba72677f411df26c9ba53381c3dbc4d
seccode <42663376+seccode@users.noreply.github.com>
2023-01-28 08:00:00 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -56,12 +56,12 @@ locs=set()
 g_key="AIzaSyA_xpbVEp9woLniuF8Cl-2TG80wkte6JiU"
 s=open("template.html").read()
 
-url=("https://api.goperigon.com/v1/all?apiKey=bf76b697-22ac-4cde-a763-0155c7822272&from=2023-01-26&sourceGroup=top100&showNumResults=true&excludeLabel=Opinion&excludeLabel=Paid News&excludeLabel=Roundup&excludeLabel=Press Release&sortBy=date&size=100")
+url=("https://api.goperigon.com/v1/all?apiKey=bf76b697-22ac-4cde-a763-0155c7822272&from=2023-01-26&sourceGroup=top100&showNumResults=true&excludeLabel=Opinion&excludeLabel=Paid News&excludeLabel=Roundup&excludeLabel=Press Release&sortBy=date&size=50")
 a=requests.get(url).json()["articles"]
+A=[]
 
 for i,article in tqdm.tqdm(enumerate(a),total=len(a)):
 	n,t,u=article["source"]["domain"],article["title"],article["url"]
-	"""
 	location=input(t+"\n")
 	if location in shortcuts:
 		location=shortcuts[location]
@@ -73,12 +73,9 @@ for i,article in tqdm.tqdm(enumerate(a),total=len(a)):
 		locs.add(l)
 	else:
 		while l in locs:
-			_lat=(random.randint(0,1000)-500)/500
-			_lng=(random.randint(0,1000)-500)/500
-			l=(l[0]+_lat,l[1]+_lng)
+			_lat=(random.randint(0,1000)-500)/1000
+			l=(l[0]+_lat,l[1])
 		locs.add(l)
-	"""
-	l=None
 
 	img=np.zeros([45,900,3],dtype=np.uint8)
 	img.fill(255)
@@ -95,6 +92,8 @@ for i,article in tqdm.tqdm(enumerate(a),total=len(a)):
 	font=ImageFont.truetype("/Library/Fonts/Arial.ttf",36)
 	draw.text(start,t,(0,0,0),font=font)
 	im.save("headlines/"+str(i)+".png")
+	s=s.replace("U"+str(i)+",",'"'+u+'"'+",")
+	s=s.replace("C"+str(i)+"}","["+str(l[1])+","+str(l[0])+"]}")
 
 f=open("index.html","w")
 f.write(s)
